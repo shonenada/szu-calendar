91a0e2ae3abdf1f3cdfddacdbf621ed5f11a7234
shonenada <shonenada@gmail.com>
2014-09-13 00:41:56 +0800

    add new message

---
+++ calendar/messages/signup.msg.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+	'min_length' => ':field 不能少于 :param2 位',
+	'max_length' => ':field 不能多于 :param2 位',
+	'not_empty' => '请输入:field',
+	'phone' => '请输入正确的:field',
+	'digit' => '请输入正确的:field',
+);
