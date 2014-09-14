b45e1218e6b8c0b0e3e38ee0ddbfaba9c6db24c0
shonenada <shonenada@gmail.com>
2014-09-14 16:16:21 +0800

    update production.sh

--- scripts/production.sh
+++ scripts/production.sh
@@ -1,7 +1,15 @@
+echo 'Removing vendor.zip'
 rm -f vendor.zip
+echo 'Removing vendor/'
 rm -rf vendor/
+echo 'Removing wwwroot/static/components'
 rm -rf wwwroot/static/components/
+echo 'Removing wwwroot/static/script/plugins'
 rm -rf wwwroot/static/scripts/plugins/
+echo 'Removing wwwroot/static/fonts'
 rm -rf wwwroot/static/fonts/
+echo 'Downloading vendor.zip'
 curl -o vendor.zip https://coding.net/u/shonenada/p/szu-calendar-vendor/git/archive/master
+echo 'unpacking'
 unzip -qo vendor.zip
+echo 'Done!'
