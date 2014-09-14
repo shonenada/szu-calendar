cc81f56e049464ce7a9142768ed34fb3870a1b43
shonenada <shonenada@gmail.com>
2014-09-14 16:14:37 +0800

    update production.sh

--- scripts/production.sh
+++ scripts/production.sh
@@ -1,4 +1,7 @@
 rm -f vendor.zip
 rm -rf vendor/
+rm -rf wwwroot/static/components/
+rm -rf wwwroot/static/scripts/plugins/
+rm -rf wwwroot/static/fonts/
 curl -o vendor.zip https://coding.net/u/shonenada/p/szu-calendar-vendor/git/archive/master
-unzip -q vendor.zip -o
+unzip -qo vendor.zip
